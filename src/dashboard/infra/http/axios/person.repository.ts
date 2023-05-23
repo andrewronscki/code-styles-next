491714c8d861807b1d07004a228cded4aa6940c4
André Wronscki <andrewronscki@gmail.com>
2023-05-23 13:57:32 -0300

    feat: Criando mock de request e tentando configurar no projeto

--- src/dashboard/infra/http/axios/person.repository.ts
+++ src/dashboard/infra/http/axios/person.repository.ts
@@ -1,7 +1,16 @@
+import { HttpClient } from "@/shared";
+
 import { PersonEntity, PersonRepository } from "@/dashboard/domain";
 
 export class AxiosPersonRepository implements PersonRepository {
+	constructor(private httpRequest: HttpClient) {}
+
 	async findAll(): Promise<PersonEntity[]> {
-		return [];
+		const people = await this.httpRequest.request({
+			method: "get",
+			url: "api/v1/people"
+		});
+
+		return people.body;
 	}
 }
--- src/dashboard/presenters/components/template/ListPeople.tsx
+++ src/dashboard/presenters/components/template/ListPeople.tsx
@@ -1,13 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { AxiosHttpClient } from "@/shared";
+
 import { FindPeople } from "@/dashboard/data";
+import { AxiosPersonRepository } from "@/dashboard/infra";
 
 import { PersonCard } from "../organisms/PersonCard";
-import { personRepositoryFactory } from "../../factories";
+import { PersonEntity } from "@/dashboard/domain";
 
 export async function ListPeople() {
-	const peopleFound = await new FindPeople(personRepositoryFactory()).execute();
+	const httpClient = new AxiosHttpClient();
+	const personRepository = new AxiosPersonRepository(httpClient);
+
+	const [people, setPeople] = useState<PersonEntity[]>([]);
+
+	useEffect(() => {
+		const findPeople = async () => {
+			const peopleFound = await new FindPeople(personRepository).execute();
+
+			setPeople(peopleFound);
+		}
+
+		findPeople();
+	}, [])
+	
 	return (
 		<div className="flex flex-wrap w-320 gap-6">
-			{peopleFound.map(card => (
+			{people.map(card => (
 				<PersonCard 
 					key={card.id} 
 					id={card.id}
