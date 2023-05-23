1ac626a7148467ae70f61dba676546170f9efd54
Eduardo Ricardo <eduwric@gmail.com>
2023-05-23 12:05:08 -0300

    chore: remove comments

--- src/dashboard/presenters/components/molecules/PersonCard.tsx
+++ src/dashboard/presenters/components/molecules/PersonCard.tsx
@@ -1,41 +1,45 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { FiChevronRight } from 'react-icons/fi';
+import { useState } from "react";
+import { FiChevronRight } from "react-icons/fi";
 import { Avatar, Button, Information, Text } from "@/shared/presenters";
-import { PersonEntity } from '@/dashboard/domain';
+import { PersonEntity } from "@/dashboard/domain";
 
-
-// This iwould be a molecule
 export function PersonCard(person: PersonEntity) {
-	const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-	function handleButton() {
-		setIsLoading(!isLoading);
-	}
+  function handleButton() {
+    setIsLoading(!isLoading);
+  }
 
-	return (
-		<div className="flex flex-col bg-white w-56 h-64 rounded-xl p-4">
-			<div className="flex justify-center items-center">
-				<Avatar alt="Imagem de perfil do usuario" height={86} width={86} border={true} rounded="rounded-full" />
-			</div>
+  return (
+    <div className="flex flex-col bg-white w-56 h-64 rounded-xl p-4">
+      <div className="flex justify-center items-center">
+        <Avatar
+          alt="Imagem de perfil do usuario"
+          height={86}
+          width={86}
+          border={true}
+          rounded="rounded-full"
+        />
+      </div>
 
-			<div className="flex justify-between w-full mt-2">
-				<div className="flex flex-col justify-start items-start w-full gap-2">
-					<Information label="Name" text={person.name} />
-					<Information label="Role" text={person.role} />
-					<Information label="Points" text={`${person.points}`} />
-				</div>
-				<div className="flex justify-end items-end">
-					<Button 
-						icon={{ type: FiChevronRight, size: 32 }} 
-						onClick={handleButton} 
-						isLoading={isLoading}
-						width='w-12'
-						height='h-12'
-					/>	
-				</div>
-			</div>
-		</div>
-	)
+      <div className="flex justify-between w-full mt-2">
+        <div className="flex flex-col justify-start items-start w-full gap-2">
+          <Information label="Name" text={person.name} />
+          <Information label="Role" text={person.role} />
+          <Information label="Points" text={`${person.points}`} />
+        </div>
+        <div className="flex justify-end items-end">
+          <Button
+            icon={{ type: FiChevronRight, size: 32 }}
+            onClick={handleButton}
+            isLoading={isLoading}
+            width="w-12"
+            height="h-12"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/dashboard/presenters/components/organisms/ListPeople.tsx
+++ src/dashboard/presenters/components/organisms/ListPeople.tsx
@@ -3,21 +3,20 @@ import { FindPeople } from "@/dashboard/data";
 import { PersonCard } from "../molecules/PersonCard";
 import { personRepositoryFactory } from "../../factories";
 
-	// This would be an organism because it has some molecules
 export async function ListPeople() {
-	const peopleFound = await new FindPeople(personRepositoryFactory()).execute();
+  const peopleFound = await new FindPeople(personRepositoryFactory()).execute();
 
-	return (
-		<div className="flex flex-wrap w-320 gap-6">
-			{peopleFound.map(card => (
-				<PersonCard 
-					key={card.id} 
-					id={card.id}
-					name={card.name}
-					role={card.role}
-					points={card.points}
-				/>
-			))}
-		</div>
-	)
+  return (
+    <div className="flex flex-wrap w-320 gap-6">
+      {peopleFound.map((card) => (
+        <PersonCard
+          key={card.id}
+          id={card.id}
+          name={card.name}
+          role={card.role}
+          points={card.points}
+        />
+      ))}
+    </div>
+  );
 }
